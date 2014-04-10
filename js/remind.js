77326dcc583d7f84213e56a1edfa98d926037560
Vaibhav Mule <vaibhavmule135@gmail.com>
2014-04-11 00:39:59 +0530

    Added the Local Storage

--- js/remind.js
+++ js/remind.js
@@ -1,4 +1,27 @@
-$('#submitButton').on('click', function (e) {
+function getAllReminders () {
+	// it will get all reminder from local storage
+	var reminders = localStorage.getItem('reminders');
+	if (!reminders) {
+		return;
+	}
+	reminders = JSON.parse(reminders);
+	for (var i = 0; i < reminders.length; i++) {
+		var text = reminders[i].text;
+		var thatTime = new Date(reminders[i].time);
+		var now = new Date();
+		var seconds = thatTime-now;
+		console.log(thatTime);
+		$('#list').append('<li>'+text+'</li>');
+		$('#text').val('');
+		setTimeout(function () {
+			// body...
+			var msg = new SpeechSynthesisUtterance(text);
+			window.speechSynthesis.speak(msg);
+		},seconds);
+	};
+
+}
+function setReminder (e) {
 	// body...
 	e.preventDefault();
 	var text = $('#text').val();
@@ -15,4 +38,27 @@ $('#submitButton').on('click', function (e) {
 		var msg = new SpeechSynthesisUtterance(text);
 		window.speechSynthesis.speak(msg);
 	},seconds);
+	// Saving the reminder to local storage
+	var reminder = { 
+		'text' : text,
+		'time' : thatTime
+	};
+	setAllReminders(reminder);
+}
+function setAllReminders (reminder) {
+	// get reminder object and saving to local storage
+	var reminders = localStorage.getItem('reminders');
+	if (!reminders) {
+		reminders = '[]';
+	}
+	reminders = JSON.parse(reminders);
+	reminders.push(reminder);
+	reminders = JSON.stringify(reminders);
+	localStorage.setItem('reminders', reminders);
+}
+$('#submitButton').on('click', setReminder);
+
+$(document).ready(function () {
+	// body...
+	getAllReminders();
 });
